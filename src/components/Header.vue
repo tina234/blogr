f0e59cc62ee651f3ae47ab7fd5db5362cd3c4170
tina234 <martina.dejanovic94@gmail.com>
2021-04-06 21:43:20 +0200

    finished dropdown

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,18 +14,18 @@
                             <div class="drop-item">Marketplace</div>
                             <div class="drop-item">Pricing</div>
                             <div class="drop-item">Freatures</div>
+                            <div class="drop-item">Integrations</div>
                         </div>
                     </div>
-                    
                     <div class="menu-item">
                         <span>Company</span>
                         <i class="fa fa-caret-down" aria-hidden="true"></i>
 
                         <div class="dropdown-list">
-                            <div class="drop-item">Overview</div>
-                            <div class="drop-item">Marketplace</div>
-                            <div class="drop-item">Pricing</div>
-                            <div class="drop-item">Freatures</div>
+                            <div class="drop-item">About</div>
+                            <div class="drop-item">Team</div>
+                            <div class="drop-item">Blog</div>
+                            <div class="drop-item">Careers</div>
                         </div>
                     </div>
                     <div class="menu-item">
@@ -33,16 +33,15 @@
                         <i class="fa fa-caret-down" aria-hidden="true"></i>
 
                         <div class="dropdown-list">
-                            <div class="drop-item">Overview</div>
-                            <div class="drop-item">Marketplace</div>
-                            <div class="drop-item">Pricing</div>
-                            <div class="drop-item">Freatures</div>
+                            <div class="drop-item">Contact</div>
+                            <div class="drop-item">Newsletter</div>
+                            <div class="drop-item">LInkedIn</div>
                         </div>
                     </div>
                 </div>
                 <div class="nav-right">
                     <p>Login</p>
-                    <button>Sign in</button>
+                    <button class="btn-sign-in">Sign in</button>
                 </div>
             </div>
         </div>
