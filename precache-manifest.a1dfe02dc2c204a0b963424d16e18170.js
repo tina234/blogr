c68ca9f67f9d2c6013f88ef4fc60a071bd7aa2fc
tina234 <martina.dejanovic94@gmail.com>
2022-06-24 17:58:02 +0200

    New Deployment

---
+++ precache-manifest.a1dfe02dc2c204a0b963424d16e18170.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdff988b8a1a7183c98",
+    "url": "/blogr/css/app.53371227.css"
+  },
+  {
+    "revision": "8ad437ddd3dd2155a148cf7c7bad431f",
+    "url": "/blogr/img/bg-pattern-circles.8ad437dd.png"
+  },
+  {
+    "revision": "7c4feeac5945b2c65472759a10b09d5e",
+    "url": "/blogr/img/bg-pattern-intro.7c4feeac.png"
+  },
+  {
+    "revision": "2b384e3bad9f2d43233d3c2783db41c7",
+    "url": "/blogr/img/illustration-editor-desktop.2b384e3b.png"
+  },
+  {
+    "revision": "995f9a555d64c74f9323255d7f54eb0b",
+    "url": "/blogr/img/illustration-laptop-desktop.995f9a55.png"
+  },
+  {
+    "revision": "8f073558d20473bca269d2f291efbde4",
+    "url": "/blogr/img/illustration-phones.8f073558.png"
+  },
+  {
+    "revision": "ce1657761fe07627a01dc9eef28f73c2",
+    "url": "/blogr/index.html"
+  },
+  {
+    "revision": "9bdff988b8a1a7183c98",
+    "url": "/blogr/js/app.cff8de6b.js"
+  },
+  {
+    "revision": "d5588fe7bb4425ceac10",
+    "url": "/blogr/js/chunk-vendors.6545af08.js"
+  },
+  {
+    "revision": "9eff3a42898c5306c0f199318522fb2c",
+    "url": "/blogr/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blogr/robots.txt"
+  }
+]);
